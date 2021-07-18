c01682d11eaef9803ff2fe19b7a034f5afbe30db
UnopposedQuill <darklightbeing@gmail.com>
2021-07-18 11:34:59 -0600

    New Split solution
    Split solution into two projects to support standalone command line compiler

--- triangle-ide/App.xaml.cs
+++ triangle-ide/App.xaml.cs
@@ -6,10 +6,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace triangle_ide_cs
+namespace triangle_ide
 {
     /// <summary>
-    /// Lógica de interacción para App.xaml
+    /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
--- triangle-ide/MainWindow.xaml.cs
+++ triangle-ide/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace triangle_ide_cs
+namespace triangle_ide
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
